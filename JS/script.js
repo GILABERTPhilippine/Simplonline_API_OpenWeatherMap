8da2062568e5122611c0b7706da5674fce93d193
PhilippineG <philippinegilabert@gmail.com>
2018-02-27 15:44:15 +0100

    optimisation du js

--- JS/script.js
+++ JS/script.js
@@ -3,36 +3,48 @@ moment.locale("fr");
 $("#date").text(moment().format('LL'));
 
 // Afficher la carte et la position
+$("#valider").click(function() {
+  var ville = $("#ville").val();
+  if (ville != ' ') {
+      $.ajax({
+          url: "http://api.openweathermap.org/data/2.5/weather?q="+ ville +"&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da",
+          type: "Get",
+          dataType: "jsonp",
+          success: function (data) {
+            var resultat = "http://api.openweathermap.org/data/2.5/weather?q="+ ville +"&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da"
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#temperature").text(reponseJson.main.temp))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#temperature").text(reponseJson.main.temp))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#tempMax").text(reponseJson.main.temp_max))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#tempMax").text(reponseJson.main.temp_max))
-  
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#tempMin").text(reponseJson.main.temp_min))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#tempMin").text(reponseJson.main.temp_min))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#presAtmos").text(reponseJson.main.pressure))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#presAtmos").text(reponseJson.main.pressure))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#vent").text(reponseJson.wind.speed))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#vent").text(reponseJson.wind.speed))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#humidite").text(reponseJson.main.humidity))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#humidite").text(reponseJson.main.humidity))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#longitude").text(reponseJson.coord.lon))
-  
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#longitude").text(reponseJson.coord.lon))
 
-window.fetch('http://api.openweathermap.org/data/2.5/weather?q=Pamiers&units=metric&lang=fr&appid=a98ee53cd7e7fdf70c48f4e96d5e60da')
-  .then(reponse => reponse.json())
-  .then(reponseJson => $("#latitude").text(reponseJson.coord.lat))
+              window.fetch(resultat)
+                  .then(reponse => reponse.json())
+                  .then(reponseJson => $("#latitude").text(reponseJson.coord.lat))
+          }
+      });
+  }
+
+});
